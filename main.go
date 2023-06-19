3e79c1ec27addf7c79641234cb090550ed5a0056
guatom999 <35306045+guatom999@users.noreply.github.com>
2023-06-20 03:19:24 +0700

    Add ForeCast Feature

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+
 	"github.com/guatom999/BadzBot/configs"
 	"github.com/guatom999/BadzBot/modules/server"
 )
@@ -9,4 +14,57 @@ func main() {
 	cfg := configs.NewConfig("./.env")
 
 	server.NewDiscordServer(cfg).Start()
+
+	resp, err := http.Get("https://catfact.ninja/fact")
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//We Read the response body on the line below.
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	_ = body
+	//Convert the body to type string
+	sb := string(body)
+	fmt.Println(sb)
 }
+
+// func testRequest() {
+
+// 	resp, err := http.Get("https://api.publicapis.org/entries")
+
+// 	if err != nil {
+// 		fmt.Println("Get error")
+// 	}
+
+// 	fmt.Println(resp)
+// }
+
+// func initDB() {
+// 	dsn := fmt.Sprintf("%v:%v@tcp(%v:%v)/%v",
+// 		viper.GetString("db.username"),
+// 		viper.GetString("db.password"),
+// 		viper.GetString("db.host"),
+// 		viper.GetString("db.port"),
+// 		viper.GetString("db.table"),
+// 	)
+
+// 	db, err := sqlx.Open(viper.GetString("db.driver"), dsn)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+
+// 	_ = db
+
+// 	resp, err := http.Get("https://api.publicapis.org/entries")
+
+// 	if err != nil {
+// 		fmt.Println("Get error")
+// 	}
+
+// 	fmt.Println(resp)
+
+// }
--- modules/botinfo/botinfoUsecases/botinfoUsecase.go
+++ modules/botinfo/botinfoUsecases/botinfoUsecase.go
@@ -16,3 +16,8 @@ func NewBotinfoUsecase() IBotinfoUsecase {
 func (u *botintoUsecase) Feature(message string) string {
 	return fmt.Sprintf("`Test Said: %v`", message)
 }
+
+func (u *botintoUsecase) Forecast() string {
+	// return fmt.Sprintf("`not avaliable now i sus: %v`", message)
+	return fmt.Sprintln("`not avaliable right now`")
+}
